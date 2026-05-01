38b9232c52873a2f3297ad6e9444d6e364d82cee
Jayasree5375 <jonnagaddala5375@gmail.com>
2026-05-01 12:28:38 +0530

    Add files via upload

---
+++ Personal information display/personal_info.py
@@ -0,0 +1,33 @@
+full_name = "Sai Deekshith Vankadara"
+age = 21
+email = "Saideekshithv@gmail.com"
+phone_number = "+91 9392523775"
+address = "Bangalore, Karnataka, India"
+
+occupation = "Software Engineer"
+skills = ["Python", "JavaScript", "HTML", "CSS", "React", "SQL", "flask"]
+hobbies = ["Coding", "Reading", "ContentCreation", "Travelling"]
+languages = ["English", "Telugu", "Hindi"]
+
+is_student = True
+is_looking_for_job = True
+is_available_for_internship = True
+is_available_for_work = True
+is_freelancer = True
+hourly_rate = 17
+
+print("Personal Information Display")
+print(f"Full Name: {full_name}")
+print(f"Age: {age} years old")
+print(f"Email: {email}")
+print(f"Phone Number: {phone_number}")  
+print(f"Address: {address}")
+print(f"Occupation: {occupation}")
+print(f"Skills:")
+print(f"is Student: {is_student}")
+print(f"is Looking for Job: {is_looking_for_job}")
+print(f"is Available for Internship: {is_available_for_internship}")
+print(f"is Available for Work: {is_available_for_work}")
+print(f"is Freelancer: {is_freelancer}")
+print(f"Hourly Rate: ${hourly_rate} per hour")    
+print(f"languages can speak: {languages}")
